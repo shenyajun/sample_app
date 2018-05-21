2c72593d6810c31a191cebadbe0d7dc564c426e1
shenyajun <shenyajundeweiruan@outlook.com>
2018-05-21 15:23:20 +0800

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login',to: 'sessions#new'
+  post '/login',to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   get '/help',to: 'static_pages#help'
   get '/about',to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -19,16 +19,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
   end
 
+
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name: "Example User",
-                                         email: "user@example.com",
-                                         password:"password",
-                                         password_confirmation: "password" } }
+    post users_path, params: { user: { name: "Example User",
+                                       email: "user@example.com",
+                                       password:"password",
+                                       password_confirmation: "password" } }
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not nil
+    assert is_logged_in?
   end
 end
