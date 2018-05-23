f4db06b06e5affe28e7003afefd0096891993708
shenyajun <shenyajundeweiruan@outlook.com>
2018-05-23 23:14:54 +0800

    new app

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
+  get 'sessions/new'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login',to: 'sessions#new'
+  post '/login',to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/help',to: 'static_pages#help'
+  get '/about',to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  root "static_pages#home"
+  resources :users
+  post '/signup', to: 'users#create'
+  resources :account_activations, only: [:edit]
+  resources :password_resets,only: [:new, :create, :edit, :update]
+end
